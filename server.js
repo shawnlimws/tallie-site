e98fc4da8b47dfbb915ae7661fef79a60d165eda
Shawn Lim <shawnlim.ws@gmail.com>
2016-02-07 17:30:47 +0800

    changed port settings

--- server.js
+++ server.js
@@ -8,5 +8,6 @@ app.get('*', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(8080)
-console.log('listening on port 8080')
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log('Listening on port 8080')
