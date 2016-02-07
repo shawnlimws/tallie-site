4a3e3c3ec66a9c35c4a4cd856d289af482bd5945
Shawn Lim <shawnlim.ws@gmail.com>
2016-02-07 17:18:54 +0800

    intial

---
+++ server.js
@@ -0,0 +1,12 @@
+var fallback = require('express-history-api-fallback')
+var express = require('express')
+var app = express()
+var root = __dirname + '/'
+app.use(express.static(root))
+// app.use(fallback('index.html', { root: root }))
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(8080)
+console.log('listening on port 8080')
